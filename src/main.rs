0a45c4ca97855f8d0867a143ea0707fdcefe4420
Anuta-loginova <anna.loginova.0219@gmail.com>
2025-08-05 17:25:07 +0300

    Implement audiomodule creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-  println!("Hello, world!");
+use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::{Device, SupportedStreamConfig};
+
+/// Инициализация аудиоустройства и конфигурации
+fn init_audio_device() -> Option<(Device, SupportedStreamConfig)> {
+    let host = cpal::default_host();
+    let device = host.default_output_device()?;
+    let config = device.default_output_config().ok()?;
+    Some((device, config))
 }
+
+fn main() {
+    let _ = init_audio_device();
+}
+ 
