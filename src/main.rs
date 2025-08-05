983d522357aaaf8c78aa943e398db23078d7c352
Vlad8002 <superdoshg@gmail.com>
2025-08-05 20:03:51 +0200

    First working window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-  println!("Hello, world!");
+use slint::slint;
+
+slint! {
+    import{Button}
+    export component App inherits Window {
+        width: 1280px;
+        height: 720px;
+        title: "Minimal Slint App";
+        }
+    }
+
+
+fn main() -> Result<(), slint::PlatformError> {
+  App::new()?.run()
 }
