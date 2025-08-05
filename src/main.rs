c1978482f37f4066302f41c4848c9ac80b394449
Anuta-loginova <anna.loginova.0219@gmail.com>
2025-08-05 22:42:05 +0300

    таксь пользовательский интерфейс весрия 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
 use slint::slint;
 
+
 slint::slint! {
-    import { Button } from "std-widgets.slint";
+    import { HorizontalBox, VerticalBox, Slider, Button } from "std-widgets.slint";
 
     export component AppWindow inherits Window {
         width: 1280px;
         height: 720px;
         title: "Minimal Slint App";
 
-        Button {
-            text: "Hi";
-            // можно добавить position и т.п.
-        }
-        Button {
-            text: "привет";
-            // можно добавить position и т.п.
+        VerticalBox {
+            spacing: 20px;
+
+            HorizontalBox {
+                spacing: 10px;
+
+                label := Text {
+                    text: "Button not clicked";
+                    font-size: 20px;
+                }
+
+                Button {
+                    text: "First";
+                    clicked => {
+                        label.text = "Button clicked";
+                    }
+                }
+
+                Button { text: "Second";          
+            
+                        }
+                Button { text: "Third"; }
+                 Slider {
+                  orientation:vertical;
+                value: 42;
+            }
+            }
+
+            HorizontalBox {
+                spacing: 10px;
+
+                Button { text: "Right 1"; }
+                Button { text: "Right 2"; }
+            }
         }
     }
 }
 
 fn main() -> Result<(), slint::PlatformError> {
-    let app = AppWindow::new()?; // <- AppWindow теперь сгенерирован из макроса выше
+    let app = AppWindow::new()?;
     app.run();
     Ok(())
 }
