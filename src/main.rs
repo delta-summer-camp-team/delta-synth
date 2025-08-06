edaff65d8db64e322b539dafa7177067e2f8f6d3
Nastia <nastionus@hmail.com>
2025-08-06 16:36:23 +0200

    New Waveforms Quadrat, Saw, Sine

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,27 @@ mod audiomodules;
 
 use audio_modules::AudioModule;
 use audiomodules::oscillator::Oscillator;
-
 use std::sync::{Arc, Mutex};
+use crate::audiomodules::oscillator::Waveforma;
 
 fn main() {
+
   let modules = build_audio_modules();
   let module = modules[0].clone();
 
 }
 
 fn build_audio_modules() -> Vec<Arc<Mutex<dyn AudioModule>>> {
-  let osc = Oscillator::new(440.0, 44100.0);
+  let osc = Oscillator::new(440.0, 44100.0, Waveforma::Quadrat, 0.5);
+  let osc1 = Oscillator::new(440.0, 44100.0, Waveforma::Sine, 0.5);
+  let osc2 = Oscillator::new(440.0, 44100.0, Waveforma::Saw, 0.5);
+
 
     vec![
-        Arc::new(Mutex::new(osc)),
+        Arc::new(Mutex::new(osc)), // Квадрат
+        Arc::new(Mutex::new(osc1)), // син
+        Arc::new(Mutex::new(osc2)), // пила
+
+
     ]
 }
