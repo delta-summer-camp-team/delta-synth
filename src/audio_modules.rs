edd880b6aa52c680f6d6e1f17dbb72ac3cfa050d
Anton <a.d.ablov@gmail.com>
2025-08-07 18:14:44 +0200

    Fix module structure

--- src/audio_modules.rs
+++
@@ -1,3 +0,0 @@
-pub trait AudioModule: Send + Sync {
-  fn process(&mut self, output: &mut [f32]);
-}
--- src/audiomodules/oscillator.rs
+++ src/audiomodules/oscillator.rs
@@ -1,4 +1,4 @@
-use crate::audio_modules::AudioModule;
+use crate::audiomodules::AudioModule;
 use std::f32::consts::PI;
 
 pub enum Waveforma {
