d6a8ed69d25e1ac7551722d02559fb5624b43e86
y7rwww <lnikulinaosnovskaa@gmail.com>
2025-08-08 20:46:00 +0300

    midi в отдельном файле и изменения в main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,16 @@ use crate::audiomodules::oscillator::Waveforma;
 use cpal::traits::{DeviceTrait, HostTrait};
 use cpal::{Device, SupportedStreamConfig};
 
+
 use anyhow::Result;
-use midir::{MidiInput, MidiInputConnection};
+use midir::MidiInputConnection;
+
 
 mod synth_state; // подключаем модуль
 use crate::synth_state::SynthState; // импортируем структуру
 
+use crate::midi_service::midi_service;
+mod midi_service;
 
 /// Инициализация аудиоустройства и конфигурации
 fn init_audio_device() -> Option<(Device, SupportedStreamConfig)> {
@@ -23,44 +27,11 @@ fn init_audio_device() -> Option<(Device, SupportedStreamConfig)> {
   Some((device, config))
 }
 
-pub fn init_synth_core() -> Result<(Arc<SynthState>, MidiInputConnection<()>)> {
-  let synth_state = SynthState::new();
-
-  let midi_in = MidiInput::new("midir reading input")?;
-  let in_ports = midi_in.ports();
-
-  if in_ports.is_empty() {
-    anyhow::bail!("Нет доступных MIDI-портов");
-  }
-
-  let in_port = &in_ports[0];
-  println!("Подключение к порту: {}", midi_in.port_name(in_port)?);
-
-  let synth_state_clone = Arc::clone(&synth_state);
-  let conn_in = midi_in.connect(
-    in_port,
-    "midir-read-input",
-    move |_, message, _| {
-      if let Some(&status) = message.first() {
-        if status & 0xF0 == 0x90 && message.len() >= 2 {
-          let key = message[1];
-          synth_state_clone.last_key.store(key, Ordering::Relaxed);
-          synth_state_clone
-            .has_key_pressed
-            .store(true, Ordering::Relaxed);
-          println!("Нажата клавиша: {}", key);
-        } else if status & 0xF0 == 0x80 && message.len() >= 2 {
-          synth_state_clone
-            .has_key_pressed
-            .store(false, Ordering::Relaxed);
-          println!("Клавиша отпущена");
-        }
-      }
-    },
-    (),
-  )?;
-
-  Ok((synth_state, conn_in))
+fn init_synth_core() -> Result<MidiInputConnection<()>> {
+    let synth_state = Arc::new(SynthState::new());
+    let conn_in = midi_service(Arc::clone(&synth_state))
+        .expect("Не удалось подключить MIDI"); // распаковываем Result
+    Ok(conn_in)
 }
 
 fn build_audio_modules() -> Vec<Arc<Mutex<dyn AudioModule>>> {
@@ -83,7 +54,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
   let modules = build_audio_modules();
   let _module = modules[0].clone();
   let _ = init_audio_device();
-  let (_synth_state, _midi_conn) = init_synth_core()?;
+  let conn_in = init_synth_core()?;
   println!("SynthState готов");
 
   loop {
