a505554fc07fe1aa7967c20aa21ec15647d13aff
Perun <egor.k.yusenko@gmail.com>
2025-08-08 15:52:37 +0200

    minor fixes and method for getting envelop

--- src/audiomodules/advanced_gate.rs
+++ src/audiomodules/advanced_gate.rs
@@ -13,46 +13,48 @@ pub struct AdvGate {
     pub sustain: u8,
     pub release: u8,
 
-    //private
     envelop: f32,
     gate_state: GateState,
 }
 
 impl AdvGate {
-    pub fn updateEnvelop(&mut self, dt: f32){ //dt = delta time
+    pub fn get_envelop(&self) -> f32 {                          //used to get envelop
+        self.envelop
+    }
 
-        fn checkUnpress(){ //checks if key is unpressed
-            if SynthState.has_key_pressed.load(Ordering::Relaxed) = false {
+    pub fn checkUnpress(&mut self) {                            //checks if key is unpressed
+            if SynthState.has_key_pressed.load(Ordering::Relaxed) == false {
                     self.gate_state = GateState::Release;
                 }
         }
 
+    pub fn updateEnvelop(&mut self, dt: f32){                   //dt = delta time
         match self.gate_state {
-            GateState::Idle => {
-                if SynthState.has_key_pressed.load(Ordering::Relaxed) = true {
+            GateState::Idle    => {                             //IDLE
+                if SynthState.has_key_pressed.load(Ordering::Relaxed) == true {
                     self.gate_state = GateState::Attack;
                 }
             }
-            GateState::Attack => {                              //ATTACK
+            GateState::Attack  => {                             //ATTACK
                 self.envelop += dt * (1.0/attack);
 
                 if self.envelop >= 1.0 {
                     self.envelop = 1.0;
                     self.gate_state = GateState::Decay;
                 }
-                checkUnpress(); //catches preemptive unpress
+                self.checkUnpress(); //catches preemptive unpress
             }
-            GateState::Decay => {                               //DECAY
+            GateState::Decay   => {                             //DECAY
                 self.envelop -= dt * (1.0/decay);
 
                 if self.envelop <= sustain {
                     self.envelop = sustain;
                     self.gate_state = GateState::Sustain;
                 }
-                checkUnpress();
+                self.checkUnpress();
             }
             GateState::Sustain => {                             //SUSTAIN
-                checkUnpress()
+                self.checkUnpress();
             }
             GateState::Release => {                             //RELEASE
                 self.envelop -= dt * (1.0/release);
