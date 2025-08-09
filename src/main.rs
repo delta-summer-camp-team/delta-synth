a71ca26eb9be3502cc98d531180646ded9d4ce65
y7rwww <lnikulinaosnovskaa@gmail.com>
2025-08-09 16:01:25 +0300

    миди в отдельном файле и изменения в мейне

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use midir::MidiInputConnection;
 mod synth_state; // подключаем модуль
 use crate::synth_state::SynthState; // импортируем структуру
 
-use crate::midi_service::midi_service;
+use crate::midi_service::initiate_midi_connection;
 mod midi_service;
 
 /// Инициализация аудиоустройства и конфигурации
@@ -27,11 +27,11 @@ fn init_audio_device() -> Option<(Device, SupportedStreamConfig)> {
   Some((device, config))
 }
 
-fn init_synth_core() -> Result<MidiInputConnection<()>> {
+fn init_synth_core() -> Result<(MidiInputConnection<()>, Arc<Arc<SynthState>>)> {
     let synth_state = Arc::new(SynthState::new());
-    let conn_in = midi_service(Arc::clone(&synth_state))
+    let conn_in = initiate_midi_connection(Arc::clone(&synth_state))
         .expect("Не удалось подключить MIDI"); // распаковываем Result
-    Ok(conn_in)
+    Ok((conn_in, synth_state))
 }
 
 fn build_audio_modules() -> Vec<Arc<Mutex<dyn AudioModule>>> {
@@ -54,7 +54,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
   let modules = build_audio_modules();
   let _module = modules[0].clone();
   let _ = init_audio_device();
-  let conn_in = init_synth_core()?;
+  let _conn_in = init_synth_core()?;
   println!("SynthState готов");
 
   loop {
