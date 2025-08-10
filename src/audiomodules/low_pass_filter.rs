761062b27b6621d1148ad0c3e2622fbe3903dbab
Norker-g <norker.ctanford@gmail.com>
2025-08-10 07:33:17 +0200

    LPF finished

--- src/audiomodules/low_pass_filter.rs
+++ src/audiomodules/low_pass_filter.rs
@@ -1,11 +1,26 @@
 pub struct low_pass_filter{
     pub cutoff: f32,
     pub res_factor: f32,
+    #[optional(default = 0)]
+    buffer1: f32,
+    #[optional(default = 0)]
+    buffer2: f32,
+
 }
 
 impl AudioModule for low_pass_filter{
-    fn diff_equation_ddy_component(y: &f32, dy: &f32, x: &f32, cutoff: &f32, res_factor: &f32){
-        let Q: f32 = 0.707;
-        - cutoff/Q *dy - cutoff.pow(2) * y + cutoff.pow(2)* x
+
+    fn y(&mut self, x:f32){
+        let dt = 1/sample_rate; 
+        let Q = self.res_factor;
+        return ((2*Q+dt*self.cutoff) * self.buffer1 + Q* y(n-2) - Q*self.cutoff*self.cutoff*dt*dt*x(n))/(Q+ dt * self.cutoff + self.cutoff*self.cutoff); //discrete solution solution of a differential equation, don't ask
+    }
+    //y''(t) + w/Q y'(t) + w^2 y(t) = w^2 x(t)
+    fn process(&mut self, output: &mut[f32]) {
+        for freq in output.iter_mut(){
+            self.buffer2 = freq;
+            freq = y(freq);
+            self.buffer1 = freq;
+        }
     }
 }
