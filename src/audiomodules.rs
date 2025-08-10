d05c4856a691b11652866cf79b2c7f738f7e8c28
Anton <a.d.ablov@gmail.com>
2025-08-10 19:01:46 +0200

    Minor fixes

--- src/audiomodules.rs
+++ src/audiomodules.rs
@@ -3,6 +3,8 @@ pub mod delay;
 pub mod gain;
 pub mod oscillator;
 pub mod advanced_gate;
+pub mod low_pass_filter;
+
 pub trait AudioModule: Send + Sync {
   fn process(&mut self, output: &mut [f32]);
 }
--- src/audiomodules/low_pass_filter.rs
+++ src/audiomodules/low_pass_filter.rs
@@ -1,9 +1,11 @@
+use crate::audiomodules::AudioModule;
+
 #[derive(Default)]
 
 pub struct LowPassFilter {
   pub cutoff: f32,
   pub res_factor: f32,
-  sample_rate: u32,
+  sample_rate: f32,
   buffer1: f32, //do not need to be specified when initing it
   buffer2: f32,
 }
@@ -13,25 +15,26 @@ impl LowPassFilter {
       cutoff,
       res_factor,
       sample_rate,
-      y1: 0.0,
-      y2: 0.0,
+      buffer1: 0.0,
+      buffer2: 0.0,
     }
   }
 
   fn filter(&mut self, x: f32) -> f32 {
     let dt: f32 = 1.0 / self.sample_rate;
-    let Q_min = 0.707;
-    let Q_max = 1.0;
-    let Q: f32 = Q_min + self.res_factor.clamp(0.0, 1.0) * (Q_max - Q_min);
-    let y: f32 = ((2 * Q + dt * self.cutoff) * self.buffer1 + Q * self.buffer2
+    let q_min = 0.707;
+    let q_max = 1.0;
+    let Q: f32 = q_min + self.res_factor.clamp(0.0, 1.0) * (q_max - q_min);
+    let y: f32 = ((2.0 * Q + dt * self.cutoff) * self.buffer1 + Q * self.buffer2
       - Q * self.cutoff * self.cutoff * dt * dt * x)
-      / (Q + dt * self.cutoff + self.cutoff * self.cutoff); //discrete solution solution of a differential equation, don't ask
+      / (Q + dt * self.cutoff + self.cutoff * self.cutoff); //discrete solution of a differential equation, don't ask
                                                             //shift state
     self.buffer2 = self.buffer1;
     self.buffer1 = y;
-    return y;
+    y
   }
 }
+
 impl AudioModule for LowPassFilter {
   fn process(&mut self, output: &mut [f32]) {
     for sample in output.iter_mut() {
