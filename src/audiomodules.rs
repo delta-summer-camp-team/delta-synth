f0d746f09d6165d401c9ab383fdc7567999b57b8
Anton <a.d.ablov@gmail.com>
2025-08-11 15:29:27 +0200

    Change filename, apply `cargo fmt --all`

--- src/audiomodules.rs
+++ src/audiomodules.rs
@@ -1,9 +1,9 @@
+pub mod advanced_gate;
 pub mod chorus;
 pub mod delay;
 pub mod gain;
-pub mod oscillator;
-pub mod advanced_gate;
 pub mod low_pass_filter;
+pub mod oscillator;
 
 pub trait AudioModule: Send + Sync {
   fn process(&mut self, output: &mut [f32]);
