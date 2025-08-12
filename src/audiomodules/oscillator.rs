caa63d562209afb356453e393a1a7258be8b536f
Nikita <milkov.nikita.09@gmail.com>
2025-08-12 13:29:16 +0100

    изменненый oscillator чтобы работал glide

--- src/audiomodules/oscillator.rs
+++ src/audiomodules/oscillator.rs
@@ -1,22 +1,23 @@
+use crate::audiomodules::glide::Glide;
 use crate::audiomodules::AudioModule;
-use std::f32::consts::PI;
-use std::sync::Arc;
 use crate::synth_state::SynthState;
+use std::f32::consts::PI;
 use std::sync::atomic::Ordering;
+use std::sync::Arc;
 
 pub struct Oscillator {
   phase: f32,
-  frequency: f32,
+  glide: Glide,
   sample_rate: f32,
   synthstate: Arc<SynthState>,
   id: usize,
 }
 
 impl Oscillator {
-  pub fn new(id: usize, frequency: f32, sample_rate: f32, synthstate:Arc<SynthState>) -> Self {
+  pub fn new(id: usize, start_freq: f32, sample_rate: f32, synthstate: Arc<SynthState>) -> Self {
     Self {
       phase: 0.0,
-      frequency,
+      glide: Glide::new(start_freq, 10.0, sample_rate),
       sample_rate,
       synthstate,
       id,
@@ -24,60 +25,60 @@ impl Oscillator {
   }
 }
 
-
 pub fn midi_note_to_freq(note: f32) -> f32 {
   if note <= 0.0 {
-        return 0.0;
-    }
-  return 440.0 * 2.0_f32.powf((note as f32 - 69.0)/12.0);
+    return 0.0;
+  }
+  return 440.0 * 2.0_f32.powf((note as f32 - 69.0) / 12.0);
 }
 
 impl AudioModule for Oscillator {
-    fn process(&mut self, output: &mut [f32]) {
+  fn process(&mut self, output: &mut [f32]) {
+    let midinota = self.synthstate.last_key.load(Ordering::Relaxed);
 
+    let gromkost = {
+      let vol = self.synthstate.gromkost.lock().unwrap();
+      vol[self.id]
+    };
 
-     let midinota = self.synthstate.last_key.load(Ordering::Relaxed);
+    let sdvig_oktov = self.synthstate.sdvig_oktov[self.id].load(Ordering::Relaxed) as f32;
+    let nnno = self.synthstate.nnno[self.id].load(Ordering::Relaxed) as f32;
 
-     
-      let gromkost = {let vol = self.synthstate.gromkost.lock().unwrap();
-            vol[self.id]};
+    let micro_zdvig = {
+      let micros = self.synthstate.micro_zdvig.lock().unwrap();
+      micros[self.id]
+    };
+    let waveforma_index = self.synthstate.waveformis[self.id].load(Ordering::Relaxed);
 
-      let sdvig_oktov = self.synthstate.sdvig_oktov[self.id].load(Ordering::Relaxed) as f32;
-      let nnno = self.synthstate.nnno[self.id].load(Ordering::Relaxed) as f32;
+    let basa_nota = midinota as f32 + sdvig_oktov * 12.0 + nnno + micro_zdvig;
 
-      let micro_zdvig = {
-        let micros = self.synthstate.micro_zdvig.lock().unwrap();
-            micros[self.id]
+    //целевая частота
+    let target_freq = midi_note_to_freq(basa_nota);
+    //перед в glide
+    self.glide.set_target(target_freq);
+    //генер плавн частоты
+    for sample in output.iter_mut() {
+      let freq = self.glide.next();
+      let phase_increment = freq / self.sample_rate;
+
+      self.phase += phase_increment;
+      if self.phase > 1.0 {
+        self.phase -= 1.0;
+      }
+      let v = match waveforma_index {
+        0 => (self.phase * 2.0 * PI).sin(),
+        1 => {
+          if (self.phase * 2.0 * PI).sin() > 0.0 {
+            1.0
+          } else {
+            -1.0
+          }
+        },
+        2 => 2.0 * self.phase - 1.0,
+        3 => 4.0 * (self.phase - 0.5).abs() - 1.0,
+        _ => 0.0,
       };
-      let waveforma_index = self.synthstate.waveformis[self.id].load(Ordering::Relaxed);
-
-
-
-      let basa_nota = midinota as f32 + sdvig_oktov * 12.0 + nnno + micro_zdvig;
-
-
-
-        self.frequency = midi_note_to_freq(basa_nota);
-
-        let phase_increment = self.frequency / self.sample_rate;
-        for sample in output.iter_mut() { 
-            self.phase += phase_increment; 
-            if self.phase > 1.0 { 
-                self.phase -= 1.0;
-            }
-            let v = match waveforma_index {
-                0 => (self.phase * 2.0 * PI).sin(),          
-                1 => if (self.phase * 2.0 * PI).sin() > 0.0 {
-                  1.0
-                }else{
-                 -1.0
-                }
-                2 => 2.0 * self.phase - 1.0,
-                3 => 4.0 * (self.phase - 0.5).abs() - 1.0,
-                _ => 0.0
-                };        
-            *sample += v * gromkost;
-            }
-
-        }
+      *sample += v * gromkost;
     }
+  }
+}
