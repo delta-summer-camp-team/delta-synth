4e8ccd55b565f18aa9b65bb2809f91576a135c21
Norker-g <norker.ctanford@gmail.com>
2025-08-12 16:33:16 +0200

    made lpf actually work. Also added it to main

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod audiomodules;
 
 use audiomodules::AudioModule;
 use audiomodules::oscillator::Oscillator;
+use audiomodules::low_pass_filter::LowPassFilter;
 use std::sync::{Arc, Mutex, atomic::{Ordering},};
 
 use anyhow::Result;
@@ -99,6 +100,7 @@ fn build_audio_modules(synthstate: Arc<SynthState>) -> Vec<Arc<Mutex<dyn AudioMo
   let osc2 = Oscillator::new(2, 880.0, 44100.0,  synthstate.clone());
   let osc3 = Oscillator::new(3, 1320.0, 44100.0,  synthstate.clone());
   //let reverbeffect = ReverbEffect
+  let lpf = LowPassFilter::new(1760.0 , 0.707 , 44100.0);
   let gate = AdvGate::new(7,7,255,7,1.0,GateState::Idle,synthstate.clone());
 
 
@@ -108,8 +110,9 @@ fn build_audio_modules(synthstate: Arc<SynthState>) -> Vec<Arc<Mutex<dyn AudioMo
     Arc::new(Mutex::new(osc2)), // пила
     Arc::new(Mutex::new(osc3)), // Триугольни
     //Arc::new(Mutex::new(reverbeffect)),
+    Arc::new(Mutex::new(lpf)),
     Arc::new(Mutex::new(gate)),
-    
+
   ]
 }
 
