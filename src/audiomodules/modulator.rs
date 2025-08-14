ca86d821fbea545d693e866cce50454c2d187d86
TiRabkin <timofey.rabkin@gamil.com>
2025-08-14 12:32:27 +0100

    Работающий (вроде как) модулятор

--- src/audiomodules/modulator.rs
+++ src/audiomodules/modulator.rs
@@ -3,12 +3,14 @@ use std::f32::consts::PI;
 pub struct Modulator {
   pub nessesary_amplitude: f32, 
   pub varying: f32,
-  pub amp: f32,
+  pub freq: f32,
+  pub step: f32
 }
  
-pub fn modulation(modulator: &mut Modulator) -> f32 {  
+pub fn modulation(modulator: &mut Modulator) -> f32 {
+  modulator.varying += modulator.step;
   if modulator.nessesary_amplitude <= 0.0 {
      return 0.0;
   } 
-  return modulator.nessesary_amplitude*(PI*modulator.varying*modulator.amp).sin();
+  return modulator.nessesary_amplitude*(PI*modulator.varying*modulator.freq).sin();
 } 
